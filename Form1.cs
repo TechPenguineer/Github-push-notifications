a6c7d33803af4d573420268fcbc43fd2c0468eff
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-18 13:33:30 -0400

    send notification

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace GithubPushNotifications
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notificationer notifier = new notificationer();
+            notifier.create_notification();
+        }
     }
 }
---
+++ notificationer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace GithubPushNotifications
+{
+    class notificationer
+    {
+        public void access_api()
+        {
+
+        }
+        public void create_new_config_option()
+        {
+
+        }
+        public void create_notification()
+        {
+            string Username,GithubRepo;
+            Username = "TechPenguineer";
+            GithubRepo = "Github-push-notifications";
+
+            string notification_type_commit = $"{Username} just made a commit to {GithubRepo}";
+            new ToastContentBuilder()
+                .AddText("Github Push Notifications")
+                .AddText(notification_type_commit)
+                .Show();
+        }
+    }
+}
